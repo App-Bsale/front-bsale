efd747127c5d63adc2dd2323a1dc946b22723f69
Bryan <mgbryan90@hotmail.com>
2023-02-01 22:08:41 -0500

    Add navbar and complete design Etapa_1

--- src/components/etapa_1.js
+++ src/components/etapa_1.js
@@ -1,40 +1,44 @@
 import { Button } from "antd";
 import "../styles/etapa_1.css";
+import Navbar from "./Navbar/index";
 
 export const Etapa1 = () => {
   return (
-    <div class="container_etapa1">
-      <div>
-        <h2> Desarrollador Web Junior</h2>
-        <h2> Etapa 1: Fundamentos de programación</h2>
-      </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-        facilis quidem rerum autem voluptas est accusantium, iste velit, cum,
-        earum sit quibusdam animi. Nulla distinctio aut repellat excepturi,
-        asperiores enim, illo similique est dolorum labore corporis qui veniam
-        rem quam animi accusamus maxime. Cum officia at voluptates, vero tempora
-        ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-        facilis quidem rerum autem voluptas est accusantium, iste velit, cum,
-        earum sit quibusdam animi. Nulla distinctio aut repellat excepturi,
-        asperiores enim, illo similique est dolorum labore corporis qui veniam
-        rem quam animi accusamus maxime. Cum officia at voluptates, vero tempora
-        ipsam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-        facilis quidem rerum autem voluptas est accusantium, iste velit, cum,
-        earum sit quibusdam animi. Nulla distinctio aut repellat excepturi,
-        asperiores enim, illo similique est dolorum labore corporis qui veniam
-        rem quam animi accusamus maxime. Cum officia at voluptates, vero tempora
-        ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-        facilis quidem rerum autem voluptas est accusantium, iste velit, cum,
-        earum sit quibusdam animi. Nulla distinctio aut repellat excepturi,
-        asperiores enim, illo similique est dolorum labore corporis qui veniam
-        rem quam animi accusamus maxime. Cum officia at voluptates, vero tempora
-        ipsam.
-      </p>
+    <>
+      <Navbar />
+      <div class="container_etapa1">
+        <div>
+          <h2> Desarrollador Web Junior</h2>
+          <h2> Etapa 1: Fundamentos de programación</h2>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+          facilis quidem rerum autem voluptas est accusantium, iste velit, cum,
+          earum sit quibusdam animi. Nulla distinctio aut repellat excepturi,
+          asperiores enim, illo similique est dolorum labore corporis qui veniam
+          rem quam animi accusamus maxime. Cum officia at voluptates, vero
+          tempora ipsam. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Eligendi facilis quidem rerum autem voluptas est accusantium,
+          iste velit, cum, earum sit quibusdam animi. Nulla distinctio aut
+          repellat excepturi, asperiores enim, illo similique est dolorum labore
+          corporis qui veniam rem quam animi accusamus maxime. Cum officia at
+          voluptates, vero tempora ipsam.Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Eligendi facilis quidem rerum autem voluptas est
+          accusantium, iste velit, cum, earum sit quibusdam animi. Nulla
+          distinctio aut repellat excepturi, asperiores enim, illo similique est
+          dolorum labore corporis qui veniam rem quam animi accusamus maxime.
+          Cum officia at voluptates, vero tempora ipsam. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Eligendi facilis quidem rerum autem
+          voluptas est accusantium, iste velit, cum, earum sit quibusdam animi.
+          Nulla distinctio aut repellat excepturi, asperiores enim, illo
+          similique est dolorum labore corporis qui veniam rem quam animi
+          accusamus maxime. Cum officia at voluptates, vero tempora ipsam.
+        </p>
 
-      <Button className="button_primary" type="primary">
-        Primary Button
-      </Button>
-    </div>
+        <Button className="button_primary" type="primary">
+          Iniciar
+        </Button>
+      </div>
+    </>
   );
 };
