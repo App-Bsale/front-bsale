5e90129c69c458f83096767ef9fe037423030b45
anthony <anthonysa0813@gmail.com>
2023-02-06 09:24:18 -0500

    first version about adminPage

--- src/App.js
+++ src/App.js
@@ -5,21 +5,29 @@ import { MultipleChoice } from "./pages/multipleChoice";
 import LoginAdmin from "./pages/admin/loginAdmin";
 import AdminDashboard from "./pages/admin";
 import FormPhaseOne from "./components/forms/formPhase1";
+import PhaseOneProvider from "./hooks/PhaseContext";
+import AllQuestions from "./pages/admin/questions";
+import PostulatesPhase1 from "./pages/admin/postulatesPhase1";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MultipleChoice />} />
-        <Route path="/login" element={<MultipleChoice />} />
-        <Route path="/loginAdmin" element={<LoginAdmin />} />
+    <PhaseOneProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MultipleChoice />} />
+          <Route path="/login" element={<MultipleChoice />} />
+          <Route path="/loginAdmin" element={<LoginAdmin />} />
 
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admin/form/phase1" element={<FormPhaseOne />} />
-        {/* <div className="App">
-        </div> */}
-      </Routes>
-    </BrowserRouter>
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/form/phase1" element={<FormPhaseOne />} />
+          <Route path="/admin/phase1/questions" element={<AllQuestions />} />
+          <Route
+            path="/admin/phase1/postulates"
+            element={<PostulatesPhase1 />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </PhaseOneProvider>
   );
 }
 
---
+++ src/pages/admin/questions.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutAdmin from "../../components/Layout/adminLayout";
+
+const AllQuestions = () => {
+  return (
+    <LayoutAdmin>
+      <h1>AllQuestions</h1>
+    </LayoutAdmin>
+  );
+};
+
+export default AllQuestions;
