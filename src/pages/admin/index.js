a43e752770cdca592be6e43110935493db80e09d
anthony <anthonysa0813@gmail.com>
2023-02-08 00:20:23 -0500

    added the logic when search user to phase1

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Button, Col, Layout, Modal, Row, Select } from "antd";
 import "./style.css";
 import { Link, NavLink } from "react-router-dom";
 import LayoutAdmin from "../../components/Layout/adminLayout";
 import FormQuestionBasic from "../../components/forms/FormQuestionBasic";
+import { getApi } from "../../services";
+import { PhaseContext } from "../../hooks/PhaseContext";
 
 const AdminDashboard = () => {
   const [isModalQuestion, setIsModalOpenQuestion] = useState(false);
   const [valueSelectType, setValueSelectType] = useState("");
+  const { phaseOneGlobal, setPhaseOneGlobal } = useContext(PhaseContext);
+  console.log(phaseOneGlobal);
+
+  useEffect(() => {
+    getApi("api/phase1").then((res) => {
+      console.log(res);
+      setPhaseOneGlobal(res[0]);
+    });
+  }, []);
 
   const showModal = () => {
     setIsModalOpenQuestion(true);
