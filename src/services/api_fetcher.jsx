e058ac04426787f66eb9c5ed939422620f7f0dfc
ever <ever864@gmail.com>
2023-02-17 16:04:09 -0500

    refactoring error message in login

--- src/services/api_fetcher.jsx
+++ src/services/api_fetcher.jsx
@@ -1,16 +1,13 @@
 // import { createGlobalStyle } from "styled-components";
 import { BASE_URL } from "../constants";
 
-
-
 export const apiFetch = async function (
   endpoint,
   method = "GET",
   headers,
   body
 ) {
-
-  console.log(endpoint, method, headers, body)
+  console.log(endpoint, method, headers, body);
 
   const response = await fetch(`${BASE_URL}${endpoint}`, {
     method,
@@ -18,14 +15,14 @@ export const apiFetch = async function (
     body: body ? JSON.stringify(body) : null,
   });
 
-
   if (!response.ok) {
     const error = await response.json();
-    throw new Error(error);
+    // throw new Error(error);
+    return error;
   }
 
   if (response.status === 204) return {};
-  const devolution  = await response.json();
-  // console.log(devolution)
-  return devolution
+  const devolution = await response.json();
+  console.log(devolution.message);
+  return devolution;
 };
